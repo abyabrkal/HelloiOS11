20f07b2f4601d4130e0bf4bb99daa0734e13ec82
ABY ABRAHAM KALATHVILA <abyabrkal@gmail.com>
2017-07-09 15:07:19 +0400

    Table View Intro Completed
    Ch8 TV completed

--- SimpleTable/SimpleTable/ViewController.swift
+++ SimpleTable/SimpleTable/ViewController.swift
@@ -8,7 +8,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    var restaurantNames = ["Cafe Deadend, Homei, Teakha", "Cafe Loisl", "Petite Oyster", "For Kee Restaurant", "Po's Atleir","Borrke St bakers", "Haigh Chcolate","Palomino Espresso", "Upstate", "Traif", "Graha Avenue", "Waffle & Wolf", "Five lewaves", "Confessional Barafina", "Donostia", "Royal Oak", "CASK Pub"]
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,7 +23,30 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // Return the nubmer of rowsa in section
+        return restaurantNames.count
+    }
 
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cellIdentifier = "Cell"
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath)
+        
+        //Configure the cell
+        cell.textLabel?.text = restaurantNames[indexPath.row]
+        cell.imageView?.image = UIImage(named: "restaurant")
+        
+        return cell
+    }
 
 }
+
+
+
+
 
